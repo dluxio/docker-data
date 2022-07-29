62c4f784b1e5971130e3bc582712ac47eaa2e7c1
Steven Ettinger <sjettingerjr@gmail.com>
2022-07-29 00:43:51 -0300

    grant

--- api/index.js
+++ api/index.js
@@ -200,14 +200,17 @@ function fetchDex(tok) {
       tickers[tok].tick = dex.hive.tick;
       tickers[tok].hbd_tick = dex.hbd.tick;
       var change = tickers[tok].tick;
-      var time = new Date().now;
-      const now = new Date().now;
+      var time = new Date().getTime();
+      const now = new Date().getTime();
+      console.log({time,now})
       for (var trade in dex.hive.his) {
         if (
           dex.hive.his[trade].t < time &&
           dex.hive.his[trade].t > now - 86400000
         ) {
+            console.log(dex.hive.his[trade].t);
           change = dex.hive.his[trade].price;
+          time = dex.hive.his[trade].t;
         }
       }
       tickers[tok].change = parseFloat(
