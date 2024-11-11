eb5d8df48d51a2446bf1ac99041799c519bb40ae
disregardfiat <sjettingerjr@gmail.com>
2024-11-11 15:40:29 -0300

    ++

--- init/CreateDB.sql
+++ init/CreateDB.sql
@@ -1,25 +1,23 @@
-```sql
 CREATE TABLE posts (
     author varchar(16) NOT NULL,
     permlink varchar(255) NOT NULL,
-    block integer,
-    votes integer,
-    voteweight integer,
-    promote integer,
-    paid integer,
-    payout integer,
+    block int,
+    votes int,
+    voteweight int,
+    promote int,
+    paid int,
+    payout int,
     payout_author varchar(16),
-    linear_weight integer,
-    voters varchar(255),
-    voters_paid varchar(255),
+    linear_weight int,
+    voters LONGTEXT,
+    voters_paid LONGTEXT,
     type varchar(16),
-    rating integer,
-    ratings integer,
-    raters varchar(255),
+    rating int,
+    ratings int,
+    raters varchar(255)
 );
 
 CREATE TABLE statssi (
     string varchar(255) NOT NULL,
     int integer,
 );
-```
